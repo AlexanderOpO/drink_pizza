c885a20642282485639f3f87c7ee3e1ddedc461a
Artem <rumyantzew.arteom@yandex.ru>
2020-05-14 14:56:49 +0300

    basket button is working

--- src/actions/addAction.js
+++ src/actions/addAction.js
@@ -3,7 +3,6 @@ import {ADD_PRODUCT_BASKET} from './types.js'
 export const addBasket = (productName) => {
     return (dispatch) => {
         console.log("Adding to basket");
-        console.log("Product: ", productName);
         dispatch({
                 type: ADD_PRODUCT_BASKET,
             });
